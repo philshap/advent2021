104cd957b102ae583e5b977100f96b241278b8dc
Phil Shapiro <philshap@gmail.com>
2021-12-11 20:30:56 -0500

    re-implement using frequencies to identify segments

--- src/day8.clj
+++ src/day8.clj
@@ -1,6 +1,5 @@
 (ns day8
-  (:require [clojure.string :as str]
-            [clojure.set :as set]))
+  (:require [clojure.string :as str]))
 
 (def input
   (-> "src/day8-input.txt"
@@ -18,6 +17,7 @@
 ;; e   f
 ;; e   f
 ;;  ggg
+;; segments is the digits 0 - 9 represented as the segments lit up when the digit is shown.
 (def segments ["abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"])
 (def unique? (->> segments (map count) frequencies (filter #(= (second %) 1)) keys (into #{})))
 
@@ -27,52 +27,23 @@
        (filter unique?)
        count))
 
-;(defn segment-frequencies [digits] (frequencies (reduce concat digits)))
-;(defn freq-total [digits]
-;  (map #(reduce + (map segment-frequencies %)) digits))
-;(def freq-total->number (zipmap (map freq-total segments) (range)))
+(defn create-ref-frequencies [digits] (frequencies (reduce concat digits)))
+(def segment-frequencies (create-ref-frequencies segments))
+(defn freq-total [ref-frequencies digits] (reduce + (map ref-frequencies digits)))
+(def freq-total->number (zipmap (map (partial freq-total segment-frequencies) segments) (range)))
 
-(defn decode [digit all-digits]
-  (let [one (first (filter #(= (count %) 2) all-digits))
-        four (first (filter #(= (count %) 4) all-digits))
-        one-overlaps (fn [digit] (count (set/intersection one digit)))
-        four-overlaps (fn [digit] (count (set/intersection four digit)))]
-    (case (count digit)
-     ; 1 - 2 segments
-     2 1
-     ; 7 - 3 segments
-     3 7
-     ; 4 - 4 segments
-     4 4
-     ; 2 - 5 segments, one overlap with 1, two overlaps with 4
-     ; 3 - 5 segments, two overlaps with 1
-     ; 5 - 5 segments, one overlap with 1, three overlaps with 4
-     5 (if (= (one-overlaps digit) 2)
-         3
-         (if (= (four-overlaps digit) 2) 2 5))
-     ; 0 - 6 segments, two overlaps with 1, three overlaps with 4
-     ; 6 - 6 segments, one overlap with 1, three overlaps with 4
-     ; 9 - 6 segments, two overlaps with 1, four overlaps with 4
-     6 (if (= (one-overlaps digit) 1)
-         6
-         (if (= (four-overlaps digit) 4) 9 0))
-     ; 8 - 7 segments
-     7 8)))
-
-(defn decode-line [line]
-  (let [code (map set (last line))
-        all-digits (map set (first line))]
+(defn decode-line [[all-digits code]]
+  (let [ref-frequencies (create-ref-frequencies all-digits)]
     (reduce (fn [output digit]
-              (+ (* 10 output) (decode digit all-digits)))
+              (+ (* 10 output) (freq-total->number (freq-total ref-frequencies digit))))
             0 code)))
 
 (defn part2 []
   (->> input
-       (map parse-line)
-       (map decode-line)
+       (map (comp decode-line parse-line))
        (reduce +)))
 
-;(comment
+(comment
   (println "part 1: " (part1))
   (println "part 2: " (part2))
-  ;)
+  )
