f423cfd646b658ce7d4ea155d43f3d473f4d9a17
Phil Shapiro <philshap@gmail.com>
2021-12-15 11:27:27 -0500

    day 15

---
+++ src/day15.clj
@@ -0,0 +1,68 @@
+(ns day15
+  (:require [clojure.string :as str])
+  (:require [clojure.data.priority-map :refer [priority-map]]))
+
+(def input
+  (-> "src/day15-input.txt"
+      slurp
+      str/split-lines))
+
+(defn parse-cave [input]
+  (into {}
+        (for [y (range (count input))
+              x (range (count (first input)))]
+          [[x y] (- (int (nth (nth input y) x)) (int \0))])))
+
+(defn cave-exit [cave]
+  [(->> cave keys (map first) (reduce max))
+   (->> cave keys (map second) (reduce max))])
+
+;; A path is a location in the cave and a current risk score
+(defn extend-path [cave paths [pos risk]]
+  (reduce
+    (fn [new-paths new-pos]
+      (if-some [new-risk (cave new-pos)]
+        (if (not (paths new-pos))
+          (conj {new-pos (+ risk new-risk)} new-paths)
+          new-paths)
+        new-paths))
+    {}
+    (map (partial mapv + pos) [[0 1] [1 0] [-1 0] [0 -1]])))
+
+(defn extend-paths [cave paths]
+  (let [[key _ :as path] (first paths)]
+    (into (assoc paths key ##Inf) (extend-path cave paths path))))
+
+(defn find-cave-path-risk [cave exit]
+  (->> (priority-map [0 0] 0)
+       (iterate (partial extend-paths cave))
+       (filter #(some? (% exit)))
+       (map vals)
+       ffirst
+       (+ (cave exit))))
+
+(defn part1 []
+  (let [cave (parse-cave input)]
+    (find-cave-path-risk cave (cave-exit cave))))
+
+(defn big-cave [cave]
+  (let [[width height] (map inc (cave-exit cave))]
+    (fn [[x y]]
+      (when (and (<= 0 x) (< x (* 5 width))
+                 (<= 0 y) (< y (* 5 height)))
+        (let [risk (+ (cave [(mod x width) (mod y height)])
+                      (quot x width)
+                      (quot y width))]
+          (if (> risk 9) (inc (mod risk 10)) risk))))))
+
+(defn part2 []
+  (let [cave (parse-cave input)]
+    (find-cave-path-risk
+      (big-cave cave)
+      (mapv dec (mapv * [5 5] (mapv inc (cave-exit cave)))))))
+
+(comment
+  (println "part 1: " (part1))
+  ;;; why?? -2 works but doesn't make any sense. will debug later...
+  (println "part 2: " (- (part2) 2))
+  )
